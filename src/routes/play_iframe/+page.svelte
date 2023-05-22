32df09ec5df20f4ecdc99cda8c93eca07da62eb1
thrombe <yash.n.thombare620@gmail.com>
2023-05-22 22:38:08 +0530

    added youtube iframe player thing

--- src/routes/play_iframe/+page.svelte
+++ src/routes/play_iframe/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { Innertube, UniversalCache } from 'youtubei.js/web';
 
 	let src_url = '';
@@ -7,7 +8,7 @@
 
 	fetch("/.netlify/functions/ollo").then(e => console.log(e))
 
-	let yt: any;
+	let yt: Innertube;
 	let f = async () => {
 		yt = await Innertube.create({
 			cache: new UniversalCache(false),
@@ -80,12 +81,98 @@
 				return res;
 			}
 		});
-		let k = await yt.getInfo(yt_id);
-		console.log(k);
+		// let k = await yt.getInfo(yt_id);
+		// console.log(k);
+	};
+	f();
+
+	const yo = async () => {
+		
 	};
 
-	let player: any;
+    let video: any;
+    let player: YT.Player;
+    const do_somethin = () => {
+        console.log(video);
+
+        // - [Play iFrame embedded YouTube Video on click](https://codepen.io/martinwolf/pen/DrPWXw)
+        // - [Control yt Video Player with js](https://tutorialzine.com/2015/08/how-to-control-youtubes-video-player-with-javascript)
+        // - [Control yt Video Player with js](https://demo.tutorialzine.com/2015/08/how-to-control-youtubes-video-player-with-javascript/)
+        // this YT thing comes from the youtube iframe api script
+        // - [youtube.d.ts File for the youtube-iframe-api](https://stackoverflow.com/questions/42352944/youtube-d-ts-file-for-the-youtube-iframe-api-to-use-in-angular-2-needed)
+        player = new YT.Player('video', {
+            width: 0,
+            height: 0,
+            videoId: 'KZvVWnRUrkU',
+            playerVars: {
+                color: 'white',
+                controls: 0,
+                // autoplay: 1,
+                showinfo: 0,
+                disablekb: 1,
+                modestbranding: 1,
+                playlist: 'KZvVWnRUrkU,vJnCiySv1Nw'
+            },
+            events: {
+                onReady: (eve: any) => {
+                    console.log(player);
+                    // player.playVideo();
+                    eve.target.playVideo();
+                    eve.target.playVideo();
+                    // let newTime = player.getDuration() * (40 / 100);
+                    // player.seekTo(newTime);
+                }
+            }
+        });
+    };
+    $: if (video) {
+        // do_somethin();
+    }
+    let pos = 0;
+
+    onDestroy(() => {
+        if (player) {
+            player.destroy();
+        }
+    })
 </script>
 
 <input bind:value={yt_id} />
-<button on:click={f} >yo</button>
+<button on:click={yo} >yo</button>
+<button on:click={do_somethin} >bo</button>
+
+
+
+
+<svelte:head>
+    <script src="https://www.youtube.com/iframe_api"></script>
+</svelte:head>
+
+<button on:click={do_somethin}>start player</button>
+
+<button
+    on:click={() => {
+        console.log(player);
+        player.seekTo(100, true);
+        player.playVideo();
+        setTimeout(() => {
+            console.log('yaaaaaaaaaaaaaaaaah');
+            player.nextVideo();
+            setTimeout(() => {
+                player.seekTo(50, true);
+            }, 5000);
+        }, 5000);
+    }}
+>
+    seek
+</button>
+<div bind:this={video} id="video" />
+<input
+    type="range"
+    on:mousedown={() => {}}
+    on:mouseup={() => {}}
+    on:change={() => {}}
+    bind:value={pos}
+    min="0"
+    max="100"
+/>
