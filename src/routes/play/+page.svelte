98a3849aa886f7818472dcf50ba98a1fbad50c7a
thrombe <yash.n.thombare620@gmail.com>
2023-05-24 01:03:19 +0530

    added basic buggy video position synchronisation

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
     import { initializeApp } from 'firebase/app';
-    import { doc, getFirestore, onSnapshot, updateDoc } from 'firebase/firestore';
+    import {
+        addDoc,
+        arrayUnion,
+        doc,
+        getFirestore,
+        onSnapshot,
+        setDoc,
+        updateDoc
+    } from 'firebase/firestore';
     import { onDestroy } from 'svelte';
     import { writable } from 'svelte/store';
     import { firebase_config } from '../../firebase-config';
@@ -23,22 +31,46 @@
     let app = initializeApp(firebase_config);
     let db = getFirestore(app);
 
+    let current_vid: string;
+    let current_pos: number;
+
     let queue = new Array();
     let playing_index: number;
+    let started_at: number;
+    let paused: boolean;
+
     let update = writable(1);
 
-    let snapshot_unsub = onSnapshot(doc(db, 'groups', group), async (doc) => {
-        let data = doc.data();
+    let snapshot_unsub = onSnapshot(doc(db, 'groups', group), async (d) => {
+        let data = d.data();
         if (!data) {
+            await setDoc(doc(db, 'groups', group), {
+                queue: [],
+                playing_index: null,
+                started_at: null,
+                paused: true
+            });
             return;
         }
         console.log(data);
         queue = data.queue;
         playing_index = data.playing_index;
+        started_at = data.started_at;
+        paused = data.paused;
         $update += 1;
     });
+    let time_sync = setInterval(async () => {
+        if (!player) {
+            return;
+        }
+        current_pos = player.getCurrentTime() / player.getDuration();
+    }, 1000);
     onDestroy(async () => {
         snapshot_unsub();
+        clearInterval(time_sync);
+        if (player) {
+            player.destroy();
+        }
     });
 
     let video: any;
@@ -51,7 +83,7 @@
             height: 0,
             playerVars: {
                 color: 'white',
-                controls: 0,
+                // controls: 0,
                 // autoplay: 1,
                 showinfo: 0,
                 disablekb: 1,
@@ -61,32 +93,80 @@
             events: {
                 onReady: (eve: any) => {
                     player = eve.target;
-                }
+                },
+                onStateChange: (eve) => {}
             }
         });
     };
 
-    $: if (playing_index || queue || $update) {
-        console.log('updateeeeeeeeeeeeeeeeeeeeeeee');
+    $: if (playing_index || queue || $update || started_at) {
         if (player) {
-            console.log('ttttttttttttttttttttttttttttt');
-            player.loadVideoById(queue[playing_index]);
-            player.playVideo();
+            // player.getPlaylist();
+            start_current_vid();
         }
     }
+
+    const start_current_vid = () => {
+        // player.loadPlaylist(queue.slice(playing_index));
+        if (current_vid !== queue[playing_index]) {
+            current_vid = queue[playing_index];
+            player.loadVideoById(current_vid);
+        }
+        if (!started_at) {
+            started_at = Date.now();
+        }
+        if (paused) {
+            console.log('pausing!!!!!!');
+            player.pauseVideo();
+        } else {
+            player.playVideo();
+            player.seekTo((Date.now() - started_at) / 1000, true);
+        }
+    };
+    const play_next = async () => {
+        if (queue.length <= playing_index) {
+            return;
+        }
+        playing_index += 1;
+        started_at = Date.now();
+        await updateDoc(doc(db, 'groups', group), {
+            playing_index: playing_index,
+            started_at: started_at
+        });
+        start_current_vid();
+    };
 
     (window as any).onYouTubeIframeAPIReady = on_yt_load;
 
     let id_input_val: string;
-    const play = async () => {
+    const queue_vid = async () => {
         if (!id_input_val) {
-            id_input_val = queue[playing_index];
+            start_current_vid();
+            return;
         }
         await updateDoc(doc(db, 'groups', group), {
-            queue: [id_input_val]
+            queue: arrayUnion(id_input_val)
         });
-        player.playVideo();
+        start_current_vid();
     };
+
+    const sync_to_this = async () => {
+        await updateDoc(doc(db, 'groups', group), {
+            started_at: started_at
+        });
+    };
+
+    const toggle_pause_vid = async () => {
+        paused = !paused;
+        await updateDoc(doc(db, 'groups', group), {
+            paused: paused
+        });
+    };
+
+    let now_time = 0;
+    setInterval(() => {
+        now_time = Date.now();
+    }, 300);
 </script>
 
 <svelte:head>
@@ -94,8 +174,14 @@
 </svelte:head>
 
 <input bind:value={id_input_val} />
-<button on:click={play}>play</button>
-{#each queue as id (id)}
+<button on:click={queue_vid}>queue</button>
+<button on:click={play_next}>next</button>
+<button on:click={sync_to_this}>sync to this</button>
+<button on:click={toggle_pause_vid}>toggle pause</button>
+
+<span>{now_time.toString().slice(8, 10)}</span>
+<span>{current_pos}</span>
+{#each queue as id, i (i)}
     <span>{id}</span>
 {/each}
 
