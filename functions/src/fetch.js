e6c523660ac6082b47c8b853baa11ff369465752
thrombe <yash.n.thombare620@gmail.com>
2023-06-04 00:39:46 +0530

    apparently Headers does not exist in nodejs. but nothing complains when running it locally  (╯°□°）╯︵ ┻━┻

--- functions/src/fetch.js
+++ functions/src/fetch.js
@@ -3,10 +3,17 @@ const fetch = require("node-fetch-commonjs");
 
 exports.handler = async (event, context) => {
     let data = JSON.parse(event.body);
+
+    let headers = {};
+    for (const header of JSON.parse(data.headers)) {
+        headers[header[0]] = header[1];
+    }
+    console.log(headers);
+    
     let request = {
         method: data.method,
         url: data.url,
-        headers: new Headers(JSON.parse(data.headers)),
+        headers,
         body: data.body,
         encoded: false,
     };
