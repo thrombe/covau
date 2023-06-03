915a0acfaea9ee8bf4119f793504fd4341fe8730
thrombe <yash.n.thombare620@gmail.com>
2023-06-04 01:57:05 +0530

    specify vite build directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
 export default defineConfig({
+	build: {
+		outDir: 'build'
+	},
 	server: {
 		port: 5178
 	},
