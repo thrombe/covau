8dd2d414b3f7084e1c3effc864ff8db61dbd57b0
thrombe <yash.n.thombare620@gmail.com>
2023-09-27 01:29:53 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    content: [
+        './src/**/*.html',
+        './src/**/*.svelte',
+        './src/*.svelte',
+        './index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
