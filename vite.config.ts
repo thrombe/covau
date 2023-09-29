7c0f33150268084cb30b675e033c2c09eaf57235
thrombe <yash.n.thombare620@gmail.com>
2023-09-29 12:57:47 +0530

    added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
+import * as path from 'path';
+import tsconfig from "./tsconfig.json";
+// import preprocess from 'svelte-preprocess';
+
+const tsconfigPathAliases = Object.fromEntries(
+	Object.entries(tsconfig.compilerOptions.paths).map(([key, values]) => {
+		let value = values[0];
+		if (key.endsWith("/*")) {
+			key = key.slice(0, -2);
+			value = value.slice(0, -2);
+		}
+
+		const nodeModulesPrefix = "node_modules/";
+		if (value.startsWith(nodeModulesPrefix)) {
+			value = value.replace(nodeModulesPrefix, "");
+		} else {
+			value = path.resolve(value);
+		}
+
+		return [key, value];
+	})
+);
 
 export default defineConfig({
 	build: {
@@ -8,6 +29,9 @@ export default defineConfig({
 	},
 	server: {
 		port: 5178
+	},
+	resolve: {
+		alias: tsconfigPathAliases,
 	},
 	plugins: [
 		svelte({
