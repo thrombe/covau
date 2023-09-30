f7e192a7f130cb73dc91de9dca6b277d5f1d9da8
thrombe <yash.n.thombare620@gmail.com>
2023-10-01 04:32:45 +0530

    fmt

--- src/lib/components/SongBrowser.svelte
+++ src/lib/components/SongBrowser.svelte
@@ -80,7 +80,8 @@
     };
 
     let tabs: Tab[] = [{ name: 'Search', searcher: song_searcher, thumbnail: null }];
-    let curr_tab = tabs[0];
+    let search_tab = tabs[0];
+    let curr_tab = search_tab;
 
     let icon_scale = 101;
 </script>
@@ -118,166 +119,166 @@
     </browse-tab-bar>
 
     {#each tabs as tab (tab.name)}
-    <browse-area class='{curr_tab == tab ? '' : 'hidden'}'>
-    {#if tab.name == 'Search'}
-        <Explorer
-            {t}
-            fac={song_fac}
-            searcher={song_searcher}
-            bind:search_query
-            bind:selected_item
-            {columns}
-            bind:item_height
-            keyboard_control={false}
-            bind:selected_item_index
-            bind:search_objects
-            bind:try_scroll_selected_item_in_view
-            {gap}
-            on_item_click={async (t) => {
-                console.log(t);
-            }}
-            let:item
-            let:selected
-        >
-            <list-item
-                class:selected={selected}
-                draggable={true}
-                on:dragstart={(event) => dragstart(event, item)}
-                on:dragend={queue_dragend}
-            >
-                <div class='item-bg'>
-                {#if item.item_type == 'song' || item.item_type == 'video'}
-                    <AudioListItem
-                        title={item.title ?? ''}
-                        title_sub={get_artist_name(item)}
-                        img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
-                        scale={icon_scale}
-                    />
-                    <button class='open-button'
-                        on:click={async () => {
-                            if (!item.id) {
-                                return;
-                            }
-                            await queue_item_add(item.id);
-                        }}
+        <browse-area class='{curr_tab == tab ? '' : 'hidden'}'>
+            {#if tab.name == search_tab.name}
+                <Explorer
+                    {t}
+                    fac={song_fac}
+                    searcher={song_searcher}
+                    bind:search_query
+                    bind:selected_item
+                    {columns}
+                    bind:item_height
+                    keyboard_control={false}
+                    bind:selected_item_index
+                    bind:search_objects
+                    bind:try_scroll_selected_item_in_view
+                    {gap}
+                    on_item_click={async (t) => {
+                        console.log(t);
+                    }}
+                    let:item
+                    let:selected
+                >
+                    <list-item
+                        class:selected={selected}
+                        draggable={true}
+                        on:dragstart={(event) => dragstart(event, item)}
+                        on:dragend={queue_dragend}
                     >
-                       Queue 
-                    </button>
-                {:else if item.item_type == 'album' || item.item_type == 'playlist'}
-                    <AudioListItem
-                        title={item.title ?? ''}
-                        title_sub={item.author?.name ?? ''}
-                        img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
-                        scale={icon_scale}
-                    />
-                    <button class='open-button'
-                        on:click={async () => {
-                            if (!item.id) {
-                                return;
-                            }
-                            let new_tab;
-                            if (item.item_type == 'album') {
-                                new_tab = {
-                                    name: 'Album: ' + item.title,
-                                    searcher: writable(await $song_fac.browse_album(item.id)),
-                                    thumbnail: item.thumbnails.length > 0 ? item.thumbnails[0].url : null,
-                                };
-                            } else {
-                                new_tab = {
-                                    name: 'Playlist: ' + item.title,
-                                    searcher: writable(await $song_fac.browse_playlist(item.id)),
-                                    thumbnail: null,
-                                };
-                            }
-                            tabs = [tabs[0], new_tab];
-                            curr_tab = new_tab;
-                        }}
+                        <div class='item-bg'>
+                            {#if item.item_type == 'song' || item.item_type == 'video'}
+                                <AudioListItem
+                                    title={item.title ?? ''}
+                                    title_sub={get_artist_name(item)}
+                                    img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
+                                    scale={icon_scale}
+                                />
+                                <button class='open-button'
+                                    on:click={async () => {
+                                        if (!item.id) {
+                                            return;
+                                        }
+                                        await queue_item_add(item.id);
+                                    }}
+                                >
+                                   Queue 
+                                </button>
+                            {:else if item.item_type == 'album' || item.item_type == 'playlist'}
+                                <AudioListItem
+                                    title={item.title ?? ''}
+                                    title_sub={item.author?.name ?? ''}
+                                    img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
+                                    scale={icon_scale}
+                                />
+                                <button class='open-button'
+                                    on:click={async () => {
+                                        if (!item.id) {
+                                            return;
+                                        }
+                                        let new_tab;
+                                        if (item.item_type == 'album') {
+                                            new_tab = {
+                                                name: 'Album: ' + item.title,
+                                                searcher: writable(await $song_fac.browse_album(item.id)),
+                                                thumbnail: item.thumbnails.length > 0 ? item.thumbnails[0].url : null,
+                                            };
+                                        } else {
+                                            new_tab = {
+                                                name: 'Playlist: ' + item.title,
+                                                searcher: writable(await $song_fac.browse_playlist(item.id)),
+                                                thumbnail: null,
+                                            };
+                                        }
+                                        tabs = [tabs[0], new_tab];
+                                        curr_tab = new_tab;
+                                    }}
+                                >
+                                    Open
+                                </button>
+                            {:else if item.item_type == 'artist'}
+                                <AudioListItem
+                                    title={item.name ?? ''}
+                                    title_sub={item.subscribers ?? ''}
+                                    img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
+                                    scale={icon_scale}
+                                />
+                                <button class='open-button'
+                                    on:click={async () => {
+                                        if (!item.id) {
+                                            return;
+                                        }
+                                        let new_tab = {
+                                            name: 'Artist: ' + item.name,
+                                            searcher: writable(await $song_fac.browse_artist_songs(item.id)),
+                                            thumbnail: null,
+                                        };
+                                        tabs = [tabs[0], new_tab];
+                                        curr_tab = new_tab;
+                                    }}
+                                >
+                                    Open
+                                </button>
+                            {/if}
+                        </div>
+                    </list-item>
+                </Explorer>
+            {:else}
+                <Explorer
+                    {t}
+                    fac={writable(null)}
+                    searcher={tab.searcher}
+                    search_query={""}
+                    {selected_item}
+                    {columns}
+                    {item_height}
+                    {selected_item_index}
+                    {search_objects}
+                    {try_scroll_selected_item_in_view}
+                    {gap}
+                    on_item_click={async (t) => {
+                        console.log(t);
+                    }}
+                    let:item
+                    let:selected
+                >
+                    <list-item
+                        class:selected={selected}
+                        draggable={true}
+                        on:dragstart={(event) => dragstart(event, item)}
+                        on:dragend={queue_dragend}
                     >
-                        Open
-                    </button>
-                {:else if item.item_type == 'artist'}
-                    <AudioListItem
-                        title={item.name ?? ''}
-                        title_sub={item.subscribers ?? ''}
-                        img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : ''}
-                        scale={icon_scale}
-                    />
-                    <button class='open-button'
-                        on:click={async () => {
-                            if (!item.id) {
-                                return;
-                            }
-                            let new_tab = {
-                                name: 'Artist: ' + item.name,
-                                searcher: writable(await $song_fac.browse_artist_songs(item.id)),
-                                thumbnail: null,
-                            };
-                            tabs = [tabs[0], new_tab];
-                            curr_tab = new_tab;
-                        }}
-                    >
-                        Open
-                    </button>
-                {/if}
-                </div>
-            </list-item>
-        </Explorer>
-    {:else}
-        <Explorer
-            {t}
-            fac={writable(null)}
-            searcher={tab.searcher}
-            search_query={""}
-            {selected_item}
-            {columns}
-            {item_height}
-            {selected_item_index}
-            {search_objects}
-            {try_scroll_selected_item_in_view}
-            {gap}
-            on_item_click={async (t) => {
-                console.log(t);
-            }}
-            let:item
-            let:selected
-        >
-            <list-item
-                class:selected={selected}
-                draggable={true}
-                on:dragstart={(event) => dragstart(event, item)}
-                on:dragend={queue_dragend}
-            >
-                <div class='item-bg'>
-                {#if item.item_type == 'song' || item.item_type == 'video'}
-                    <AudioListItem
-                        title={item.title ?? ''}
-                        title_sub={get_artist_name(item)}
-                        img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : curr_tab.thumbnail ?? ''}
-                        scale={icon_scale}
-                    />
-                    <button class='open-button'
-                        on:click={async () => {
-                            if (!item.id) {
-                                return;
-                            }
-                            await queue_item_add(item.id);
-                        }}
-                    >
-                       Queue 
-                    </button>
-                {:else}
-                    <AudioListItem
-                        title={'unknown item'}
-                        title_sub={''}
-                        img_src={''}
-                        scale={icon_scale}
-                    />
-                {/if}
-                </div>
-            </list-item>
-        </Explorer>
-    {/if}
-    </browse-area>
+                        <div class='item-bg'>
+                            {#if item.item_type == 'song' || item.item_type == 'video'}
+                                <AudioListItem
+                                    title={item.title ?? ''}
+                                    title_sub={get_artist_name(item)}
+                                    img_src={item.thumbnails.length > 0 ? item.thumbnails[0].url : curr_tab.thumbnail ?? ''}
+                                    scale={icon_scale}
+                                />
+                                <button class='open-button'
+                                    on:click={async () => {
+                                        if (!item.id) {
+                                            return;
+                                        }
+                                        await queue_item_add(item.id);
+                                    }}
+                                >
+                                   Queue 
+                                </button>
+                            {:else}
+                                <AudioListItem
+                                    title={'unknown item'}
+                                    title_sub={''}
+                                    img_src={''}
+                                    scale={icon_scale}
+                                />
+                            {/if}
+                        </div>
+                    </list-item>
+                </Explorer>
+            {/if}
+        </browse-area>
     {/each}
 </div>
 
